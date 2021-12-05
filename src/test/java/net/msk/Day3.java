b45e2523fe794866dd18514604a47a5d4f922fb4
msk84 <florian@steinle.it>
2021-12-05 23:56:46 +0100

    Added Day3 data

---
+++ src/test/java/net/msk/Day3.java
@@ -0,0 +1,14 @@
+package net.msk;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+public class Day3 {
+
+    @Test
+    public void task1Test() throws IOException, URISyntaxException {
+
+    }
+}
